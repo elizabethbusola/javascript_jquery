fb0210a0a5ae3a0c54c2bf022649d855154ffbae
elizabethbusola <lizzykemmie@gmail.com>
2018-11-18 19:41:38 +0100

    personal details added

---
+++ quiz2js.js
@@ -0,0 +1,12 @@
+var person = {
+name: ['Busola', 'Adedoyin'],
+age: 30,
+gender: "Female",
+interests: ['Software Development', 'Music'],
+bio: function() {
+alert(this.name[0] + ' ' + this.name[1] + ' is ' + this.age + ' years old. She likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
+  	},
+  	greeting: function() {
+    	alert('Hi! I\'m ' + this.name[0] + '.');
+  	}
+};
